00879d9217a5a2652ecc7920c6a3c400dcf8b71f
KenanyaKAP <69131654+KenanyaKAP@users.noreply.github.com>
2022-03-27 19:34:25 +0700

    Update GUI

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include <stdlib.h>
 
 #include "include/data.hpp"
@@ -8,64 +9,45 @@ using namespace std;
 
 int main()
 {
-	string temp;
-
 	Data data;
+
+	// data.addUser("admin", "Admin", "adminId", User::Role::Admin);
 
 	string usernameInput;
 	string passwordInput;
 
-	// while (1)
-	// {
-	// 	system("cls");
-	// 	// cout << "░██████╗██╗░█████╗░██╗░░██╗░█████╗░██████╗░" << endl;
-	// 	// cout << "██╔════╝██║██╔══██╗██║░██╔╝██╔══██╗██╔══██╗" << endl;
-	// 	// cout << "╚█████╗░██║███████║█████═╝░███████║██║░░██║" << endl;
-	// 	// cout << "░╚═══██╗██║██╔══██║██╔═██╗░██╔══██║██║░░██║" << endl;
-	// 	// cout << "██████╔╝██║██║░░██║██║░╚██╗██║░░██║██████╔╝" << endl;
-	// 	// cout << "╚═════╝░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░" << endl;
-	// 	cout << "by Kenanya Keandra Adriel Prasetyo" << endl << endl << endl;
-	// 	cout << "Username/ID : ";
-	// 	cin >> usernameInput;
-	// 	cout << "Password    : ";
-	// 	cin >> passwordInput;
-
-	// 	// Check is username exist
-	// 	// if (auto user = data.getUserByUsername(usernameInput))
-	// 	// {
-	// 	// 	cout << "Username good" << endl;
-	// 	// 	if (user->getPassword() == passwordInput)
-	// 	// 	{
-	// 	// 		cout << "Success Login!" << endl;
-	// 	// 	}
-	// 	// }
-	// 	// else
-	// 	// {
-	// 	// 	cout << "Account not found!" << endl;
-	// 	// }
-	// 	cin.ignore();
-	// }
-	// char s1[] = "admin1";
-	// char s2[] = "Admin1";
-	// char s3[] = "adminId";
-	// data.addUser(s1, s2, s3, User::Role::Admin);
-
-	// data.addUser("admin", "Admin", "adminId", User::Role::Admin);
-	// data.addUser("admin2", "Admin2", "adminId", User::Role::Admin);
-	// data.addUser("admin3", "Admin3", "adminId", User::Role::Admin);
-	// data.addUser("admin4", "Admin4", "adminId", User::Role::Admin);
-	// data.addUser("admin5", "Admin5", "adminId", User::Role::Admin);
-
-	// data.removeUser((char*)"admin1");
-
-	vector<User>* users = data.getAllUser();
-
-	for (User& toto : *users)
+	while (1)
 	{
-		cout << toto.getUsername() << " | ";
-		cout << toto.getPassword() << " | ";
-		cout << toto.getPersonId() << " | ";
-		cout << toto.getRole() << endl;
+		system("cls");
+		cout << " ____  _       _             _ " << endl;
+		cout << "/ ___|(_) __ _| | ____ _  __| |" << endl;
+		cout << "\\___ \\| |/ _` | |/ / _` |/ _` |" << endl;
+		cout << " ___) | | (_| |   < (_| | (_| |" << endl;
+		cout << "|____/|_|\\__,_|_|\\_\\__,_|\\__,_|" << endl << endl;
+		cout << "by Kenanya" << endl << endl << endl;
+		cout << "Username/ID : ";
+		cin >> usernameInput;
+		cout << "Password    : ";
+		cin >> passwordInput;
+
+		// Check is username exist
+		if (auto user = data.getUser((char*)usernameInput.c_str()))
+		{
+			cout << "Username good" << endl;
+			if (strcmp(user->getPassword(), "Admin "))
+			{
+				cout << "Success Login!" << endl;
+			}
+			cout << user->getPassword();
+		}
+		else
+		{
+			cout << "Account not found!" << endl;
+		}
+		cin.ignore();
+		cin.ignore();
+		cin.ignore();
+		cin.ignore();
 	}
 
 	cout << "Done";
